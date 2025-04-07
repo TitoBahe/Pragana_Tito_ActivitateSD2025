e8a5853a138e306072cf405cbf70476778a6fed3
titobahe <titobahe@gmail.com>
2025-04-07 22:07:00 +0300

    v1

--- seminar3YT/seminar3.c
+++ seminar3YT/seminar3.c
@@ -35,32 +35,44 @@ void adaugaMasinaInVector(Masina** masini, int * nrMasini, Masina masinaNoua) {
 	//adauga in vectorul primit o noua masina pe care o primim ca parametru
 	//ATENTIE - se modifica numarul de masini din vector;
     Masina* m = (Masina*)malloc(sizeof(Masina) * ((*nrMasini)+1));
-    int nrMasiniOld = (*nrMasini);
 
-    for(int i = 0; i < nrMasini; i++){
+    for(int i = 0; i < (*nrMasini); i++){
         m[i] = (*masini)[i];
-        m[i].model = (char*)malloc(sizeof(char)*(strlen((*masini)[i].model)+1));
-        strcpy(m[i].model, (*masini)[i].model);
     }
-    m[*nrMasini] = masinaNoua;
-    m[*nrMasini].model = (char*)malloc(sizeof(char)* (strlen(masinaNoua.model)+1));
-    strcpy(m[*nrMasini].model, masinaNoua.model);
+    m[(*nrMasini)] = masinaNoua;
 
-    dezalocareVectorMasini(masini, nrMasini);
-
-    *masini = (Masina*)malloc(sizeof(Masina) * ((nrMasiniOld)+1));
-
-    for(int i = 0; i < nrMasiniOld; i++){
-        (*masini)[i] = m[i];
-        (*masini)[i].model = (char*)malloc(sizeof(char)*(strlen(m[i].model)+1));
-        strcpy((*masini)[i].model, m[i].model);
-    }
+    free((*masini));
+    (*masini) = m;
+    (*nrMasini)++;
 
 }
 
 Masina citireMasinaFisier(FILE* file) {
 	//functia citeste o masina dintr-un strceam deja deschis
 	//masina citita este returnata;
+    Masina m;
+    char buffer[100];
+    char sep[4]=",;\n";
+    fgets(buffer, 100, file);
+    printf("Buffer: %s\n", buffer);
+    m.id = atoi(strtok(buffer, sep)); //usamos o _CRT_SECURE_NO_WARNINGS
+    //atoi -> Ascii TO Integer
+    m.nrUsi = atoi(strtok(NULL, sep)); //damos NULL aqui para ele continuar de onde parou, se dermos buffer de novo, ele vai dar o primeiro item de novo
+    m.pret= atof(strtok(NULL, sep));
+
+    char*aux;
+    aux = strtok(NULL, sep); //usamos um char* aqui pq nao sabemos o tamanho da string e precisamos 
+    m.model = (char*)malloc(sizeof(char)*(strlen(aux)+1)); //agora aqui podemos alocar a quant certa pq temos o valor no aux
+    strcpy(m.model, aux);
+
+    aux = strtok(NULL, sep);
+    m.numeSofer = (char*)malloc(sizeof(char)*(strlen(aux)+1));
+    strcpy(m.numeSofer, aux);
+
+    m.serie = strtok(NULL, sep)[0];//usamos [0] pq strtok returna um char* entao pegamos o primeiro elemenot que e nosso char
+
+    return m;
+
 }
 
 Masina* citireVectorMasiniFisier(const char* numeFisier, int* nrMasiniCitite) {
@@ -68,27 +80,37 @@ Masina* citireVectorMasiniFisier(const char* numeFisier, int* nrMasiniCitite) {
 	//prin apelul repetat al functiei citireMasinaFisier()
 	//numarul de masini este determinat prin numarul de citiri din fisier
 	//ATENTIE - la final inchidem fisierul/stream-ul
+
+    FILE* file = fopen(numeFisier, "r");
+    Masina *masini = NULL;
+    (*nrMasiniCitite) = 0;
+    while(!feof(file)){
+        adaugaMasinaInVector(&masini, nrMasiniCitite, citireMasinaFisier(file));
+    } 
+    fclose(file);
+    return masini;
 }
 
 void dezalocareVectorMasini(Masina** vector, int* nrMasini) {
 	//este dezalocat intreg vectorul de masini
     for(int i  = 0; i < (*nrMasini); i++){
-        free((*vector)[i].model);
+        if((*vector)[i].model != NULL){
+            free((*vector)[i].model);
+        }
+        if((*vector)[i].numeSofer != NULL){
+            free((*vector)[i].numeSofer);
+        }
     }
     free(*vector);
-    *nrMasini = 0;
-    *vector = NULL;
+    (*nrMasini) = 0;
+    (*vector) = NULL;
 }
 
 int main() {
-    Masina m;
-    m.id = 1;
-    m.nrUsi = 4;
-    m.model = "Dacia";
-    m.numeSofer = "Tito";
-    m.pret = 24000.5;
-    m.serie = 'A';
-    afisareMasina(m);
-
+    Masina* m;
+    int nrMasiniCitite = 0;
+    m = citireVectorMasiniFisier("masini.txt", &nrMasiniCitite);
+    afisareVectorMasini(m, nrMasiniCitite);
+    dezalocareVectorMasini(&m, &nrMasiniCitite);
 	return 0;
 }
